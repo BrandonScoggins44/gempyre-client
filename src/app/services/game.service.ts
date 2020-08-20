c2ba3d0da2d41445d5a4973cdc3097813b88640a
Brandon Scoggins <brandon.scoggins44@gamil.com>
2020-08-20 15:57:12 -0400

    created tier cards. Added shuffle

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -23,9 +23,12 @@ export class GameService {
 
   public buildGame(): void {
     this.generateNobles();
+    this.generateT3Cards();
+    this.generateT2Cards();
+    this.generateT1Cards();
   }
 
-  public generateNobles(): void {
+  private generateNobles(): void {
     console.log('generateNobles')
 
     this.nobles = []                                                                                              // clear nobles incase of new game
@@ -35,30 +38,13 @@ export class GameService {
       availableTypes.pop()                                                                                        // remove the last gem type, "GOLD", to genereate noble cost
 
       let cost: Gem[] = [];
-      let index;
 
       switch (Math.floor(Math.random() * 2)) {                                                                    // 0: 2 4 point costs, 1: 3 3 point costs
         case 0:
-          index = Math.floor(Math.random() * availableTypes.length);                                              // get random index value from available gem types
-          cost.push({ type: GemType[availableTypes[index]], value: 4});                                           // create a cost using random type
-          availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)            // remove used gem type so it is not repeated
-          
-          index = Math.floor(Math.random() * availableTypes.length);                                              // repeat using remaing types
-          cost.push({ type: GemType[availableTypes[index]], value: 4});
-          availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)
+          this.generateCost(cost, 4, 2)
           break;
         case 1:
-          index = Math.floor(Math.random() * availableTypes.length);                                              // described above
-          cost.push({ type: GemType[availableTypes[index]], value: 3});
-          availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)
-          
-          index = Math.floor(Math.random() * availableTypes.length);                                              // described above
-          cost.push({ type: GemType[availableTypes[index]], value: 3});
-          availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)
-          
-          index = Math.floor(Math.random() * availableTypes.length);                                              // described above
-          cost.push({ type: GemType[availableTypes[index]], value: 3});
-          availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)
+          this.generateCost(cost, 3, 3)
           break;
         default:
           console.log('default');
@@ -67,7 +53,116 @@ export class GameService {
 
       this.nobles.push(new Noble({ id: i, points: 3, cost }));
     }
+    this.shuffleCards(this.nobles)
     console.log('nobles', this.nobles)
+  }
+
+  private generateT3Cards(): void {
+    console.log('generateT3Cards')
+
+    this.t3Cards = []                                                                                               // clear card array for new game
+
+    let availableTypes = Object.keys(GemType).slice(0, Object.keys(GemType).length - 1)                             // get gem types excluding gold
+
+    for (var i = 0; i < 2; i++) {                                                                                   // number of times each gem type should be made
+      for (var j = 0; j < availableTypes.length; j++) {                                                             // create a card for each gem type
+        let cost: Gem[] = [];                                                                                       // clear cost for new card
+
+        let id = i * availableTypes.length + j;                                                                     // dynamic sequential id
+        let value = { type: GemType[availableTypes[j]], value: 1 };                                                 // gem value. Each card is worth 1 type of gem
+        this.generateCost(cost, Math.floor(Math.random() * 6) + 7);                                                 // generate cost with random value (7-12) 
+        let points = Math.floor(Math.random() * 2) + 3;                                                             // randomize points (3-4)
+
+        this.t3Cards.push(new Tier3Card({ id, value, cost, points }))                                               // add card to deck
+      }
+    }
+    this.shuffleCards(this.t3Cards)                                                                                 // shuffle cards
+    console.log('t3Cards', this.t3Cards)
+  }
+
+  private generateT2Cards(): void {
+    console.log('generateT2Cards')
+
+    this.t2Cards = []                                                                                               // clear card array for new game
+
+    let availableTypes = Object.keys(GemType).slice(0, Object.keys(GemType).length - 1)                             // get gem types excluding gold
+
+    for (var i = 0; i < 4; i++) {                                                                                   // number of times each gem type should be made
+      for (var j = 0; j < availableTypes.length; j++) {                                                             // create a card for each gem type
+        let cost: Gem[] = [];                                                                                       // clear cost for new card
+
+        let id = i * availableTypes.length + j;                                                                     // dynamic sequential id
+        let value = { type: GemType[availableTypes[j]], value: 1 };                                                 // gem value. Each card is worth 1 type of gem
+        this.generateCost(cost, Math.floor(Math.random() * 4) + 5);                                                 // generate cost with random value (5-8) 
+        let points = Math.floor(Math.random() * 2) + 1;                                                             // randomize points (1-2)
+
+        this.t2Cards.push(new Tier2Card({ id, value, cost, points }))                                               // add card to deck
+      }
+    }
+    this.shuffleCards(this.t2Cards)                                                                                 // shuffle cards
+    console.log('t2Cards', this.t2Cards)
+  }
+
+  private generateT1Cards(): void {
+    console.log('generateT1Cards')
+
+    this.t1Cards = []                                                                                               // clear card array for new game
+
+    let availableTypes = Object.keys(GemType).slice(0, Object.keys(GemType).length - 1)                             // get gem types excluding gold
+
+    for (var i = 0; i < 6; i++) {                                                                                   // number of times each gem type should be made
+      for (var j = 0; j < availableTypes.length; j++) {                                                             // create a card for each gem type
+        let cost: Gem[] = [];                                                                                       // clear cost for new card
+
+        let id = i * availableTypes.length + j;                                                                     // dynamic sequential id
+        let value = { type: GemType[availableTypes[j]], value: 1 };                                                 // gem value. Each card is worth 1 type of gem
+        this.generateCost(cost, Math.floor(Math.random() * 4) + 1);                                                 // generate cost with random value (1-4) 
+        let points = Math.floor((Math.random() + .25));                                                             // randomize points (0-1) weighted toward 0
+
+        this.t1Cards.push(new Tier1Card({ id, value, cost, points }))                                               // add card to deck
+      }
+    }
+    this.shuffleCards(this.t1Cards)                                                                                 // shuffle cards
+    console.log('t1Cards', this.t1Cards)
+  }
+
+  private generateCost(cost: Gem[], value: number, numOfCosts?: number): void {
+    console.log('generateCost')
+
+    let availableTypes = Object.keys(GemType)                                                                       // get all gem types from enum
+    availableTypes.pop()                                                                                            // remove the last gem type, "GOLD", to genereate cost
+
+    if (numOfCosts) {
+      for (var i = 0; i < numOfCosts; i++) {
+        let index = Math.floor(Math.random() * availableTypes.length);                                              // get random index value from available gem types
+        cost.push({ type: GemType[availableTypes[index]], value });                                                 // create a cost using random type
+        availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)                // remove used gem type so it is not repeated
+      }
+    } else {
+      let numOfCostTypes = Math.floor(Math.random() * 4) + 1;                                                       // cards should have 1-4 different gem type costs
+      let numOfTokens = 0;                                                                                          // initialize number of gems used
+
+      for (var i = 0; i < numOfCostTypes; i++) {                                                                    // create a cost this many times
+
+        let max = value - numOfCostTypes + 1 + i;                                                                   // max value a cost can be taking into account how many gem types are on the card and the total cost that was passed or remaining
+        let min = (i + 1) == numOfCostTypes ? max : 1;                                                              // min value a cost can be. Will be either 1 or equal to the max value if it is the last gem type
+
+        numOfTokens = Math.floor(Math.random() * (max - min + 1)) + min;                                            // how much this gem type will be in the total cost
+
+        value -= numOfTokens;                                                                                       // reduce the remaining value by the number of gems being assigned to this cost
+
+        let index = Math.floor(Math.random() * availableTypes.length);                                              // get random index value from available gem types
+        cost.push({ type: GemType[availableTypes[index]], value: numOfTokens });                                    // create a cost using random type
+        availableTypes = availableTypes.filter(type => GemType[type] !== cost[cost.length - 1].type)                // remove used gem type so it is not repeated
+      }
+    }
+  }
+
+  private shuffleCards(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
   }
 
   public getNobles(): Noble[] {
