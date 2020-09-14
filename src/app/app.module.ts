3e799f00aadbc62c35b416bdd245d1b2809dd540
Brandon Scoggins <brandon.scoggins44@gamil.com>
2020-09-14 16:44:10 -0400

    turn action popover started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { PlayComponent } from './components/play/play.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { RulesComponent } from './components/rules/rules.component';
 import { ForumsComponent } from './components/forums/forums.component';
+import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlayComponent } from './components/play/play.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RulesComponent } from './components/rules/rules.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { LogoutComponent } from './components/logout/logout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
